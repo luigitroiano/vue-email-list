c4956706cfb22b89c3523a33bd69394cbe73751b
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-08 17:49:12 +0100

    Esercizio Completato

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,16 @@
 let app = new Vue({
   el: "#root",
   data: {
+    emails: [],
 
   },
-  
+  mounted: function() {
+    for (let i = 0; i < 9; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((respo) => {
+        const email = respo.data.response;
+        this.emails.push(email);
+      });
+    }
+  },
 });
